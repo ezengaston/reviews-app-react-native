7ed9b46260039c94be1555ffee7a17877191b44e
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-02-04 22:33:39 +0000

    Added custom font family

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 import Home from "./screens/home";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isFontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isFontsLoaded]);
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Home />
     </View>
   );
