4b698a6328cd6ed7c2e01dd66606bdea332172c6
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-02-05 17:26:58 +0000

    Added reviews

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View } from "react-native";
 
 import { globalStyles } from "../styles/global";
 
 export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
-
   return (
     <View style={globalStyles.container}>
-      <Text>Review details screen</Text>
-      <Button title="back to home screen" onPress={pressHandler} />
+      <Text>{navigation.getParam("title")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+      <Text>{navigation.getParam("rating")}</Text>
     </View>
   );
 }
