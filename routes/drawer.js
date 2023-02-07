d8dcfe171323f02effe8690ee785db5b5f2ecd17
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-02-07 16:59:43 +0000

    Added background image and title image to drawer navigation

--- routes/drawer.js
+++ routes/drawer.js
@@ -1,5 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { HeaderTitle } from "@react-navigation/elements";
+import { StyleSheet, Text, Image, View } from "react-native";
 
 import HomeStack from "./homeStack";
 import About from "../screens/about";
@@ -14,19 +16,50 @@ export default function MyDrawer() {
           headerStyle: {
             backgroundColor: "#eee",
           },
+          headerTitle: () => (
+            <HeaderTitle>
+              <View style={styles.headerContainer}>
+                <Image
+                  style={styles.headerImage}
+                  source={require("../assets/heart_logo.png")}
+                />
+                <Text style={styles.headerTitle}>GameZone</Text>
+              </View>
+            </HeaderTitle>
+          ),
+          headerBackground: () => (
+            <Image
+              style={styles.headerBackground}
+              source={require("../assets/game_bg.png")}
+            />
+          ),
         }}
       >
-        <Drawer.Screen
-          name="Home"
-          component={HomeStack}
-          options={{ headerTitle: "GameZone" }}
-        />
-        <Drawer.Screen
-          name="About"
-          component={About}
-          options={{ headerTitle: "GameZone" }}
-        />
+        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen name="About" component={About} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#333",
+    letterSpacing: 1,
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
+  },
+  headerBackground: {
+    width: "100%",
+    height: "100%",
+  },
+});
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -10,7 +10,7 @@ export default function HomeStack() {
     <Stack.Navigator
       screenOptions={{
         headerStyle: {
-          backgroundColor: "#eee",
+          backgroundColor: "#fafafa",
         },
       }}
     >
