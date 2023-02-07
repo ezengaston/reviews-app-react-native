00617ca451bcdfdcaa04e1868122fe9d30924b78
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-02-07 16:12:52 +0000

    Added review images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 import Card from "../shared/card";
 
-import { globalStyles } from "../styles/global";
+import { globalStyles, images } from "../styles/global";
 
 export default function ReviewDetails({ route }) {
   const params = route.params;
+  const rating = params.rating;
 
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{params.title}</Text>
         <Text>{params.body}</Text>
-        <Text>{params.rating}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+});
